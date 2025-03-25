bedcbb190c56aebd00358d890ef06383e4b2eac3
HaroldBarraza <132396790+HaroldBarraza@users.noreply.github.com>
2025-03-25 12:55:20 -0500

    search

--- finalproject/app/ui/invoices/search.tsx
+++ finalproject/app/ui/invoices/search.tsx
@@ -1,41 +1,36 @@
 'use client'; 
-import { useSearchParams, usePathname, useRouter} from 'next/navigation';
-import '@/app/ui/invoices/search.css'
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import '@/app/ui/invoices/search.css';
 
-export default function Search({placeholder}: {placeholder: string}){
+export default function Search({ placeholder }: { placeholder: string }) {
+    const searchParams = useSearchParams(); // Obtiene los parámetros de búsqueda de la URL
+    const pathname = usePathname(); // Obtiene la ruta actual
+    const { replace } = useRouter(); // Obtiene la función replace para cambiar la URL
 
-    const searchParams = useSearchParams();
-    const pathname = usePathname();
-    const {replace} = useRouter();
-
-    function handleSearch(term:string){
+    function handleSearch(term: string) {
         console.log(`Searching... ${term}`);
-        const params = new URLSearchParams(searchParams);
-        params.set('page', '1')
-        if(term){
-          params.set('query', term);
-        }else{
-          params.delete('query');
+        const params = new URLSearchParams(searchParams); // Crea un nuevo objeto URLSearchParams a partir de los parámetros de búsqueda actuales
+        params.set('page', '1'); // Establece la página a 1
+        if (term) {
+            params.set('query', term); // Si hay un término de búsqueda, lo establece
+        } else {
+            params.delete('query'); // Si no hay término, elimina el parámetro de búsqueda
         }
-        replace(`${pathname}?${params.toString()}`)
-      }
+        replace(`${pathname}?${params.toString()}`); // Reemplaza la URL con los nuevos parámetros
+    }
 
-    return(
+    return (
         <div className="totalsearch">
-        <label htmlFor="search" className="sr-only">
-        </label>
-        <img src='/lupa.png'alt='search' width={10}/>
-        <input
-          className="searchbar"
-          placeholder={placeholder}
-          onChange={(e) => {
-            handleSearch(e.target.value)
-          }}
-          defaultValue={searchParams.get('query')?.toString()}
-        />
-        
-      </div>
-    )
+            <label htmlFor="search" className="sr-only">Search</label>
+            <img src='/lupa.png' alt='search' width={10} />
+            <input
+                className="searchbar"
+                placeholder={placeholder}
+                onChange={(e) => {
+                    handleSearch(e.target.value); // Llama a handleSearch en el cambio del input
+                }}
+                defaultValue={searchParams.get('query')?.toString()} // Establece el valor por defecto del input
+            />
+        </div>
+    );
 }
-
-
